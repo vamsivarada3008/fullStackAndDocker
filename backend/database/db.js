9cc11843ace2db2aece71204f654335c75190f69
vamsi <vamsivarada3008@gmail.com>
2023-04-24 19:07:51 +0530

    url has been updated

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb+srv://vamsivarada3008:Vamsee135@cluster0.a79d8ip.mongodb.net/'
+    db: 'mongodb+srv://vamsivarada3008:Vamsee135@cluster0.a79d8ip.mongodb.net/reactdb'
   };
