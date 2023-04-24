43bc406e30e348f9ed87d6dab3d2fad6ad7e18c5
vamsi <vamsivarada3008@gmail.com>
2023-04-24 16:40:11 +0530

    mongo url changed

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb+srv://vamsivarada3008:Vamsee135@cluster0.a79d8ip.mongodb.net/blog'
+    db: 'mongodb+srv://vamsivarada3008:Vamsee135@cluster0.a79d8ip.mongodb.net/'
   };
